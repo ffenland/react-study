5dfdfee312a7a1b637d74f9b9679a46450be573c
ffenland <ffenland@gmail.com>
2023-05-16 15:51:45 +0900

    env

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,11 @@
 const About = () => {
-  return <h1>About</h1>;
+  console.log(process.env.NAVER_CLIENT_ID);
+  return (
+    <div>
+      <span>{process.env.NAVER_CLIENT_SECRET}</span>
+      <span>{process.env.NAVER_CLIENT_ID}</span>
+    </div>
+  );
 };
 
 export default About;
