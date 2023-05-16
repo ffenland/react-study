bf1343792fda22a4836aad428e10249494b78293
ffenland <ffenland@gmail.com>
2023-05-16 15:54:22 +0900

    dd

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,8 +2,8 @@ const About = () => {
   console.log(process.env.NAVER_CLIENT_ID);
   return (
     <div>
-      <span>{process.env.NAVER_CLIENT_SECRET}</span>
-      <span>{process.env.NAVER_CLIENT_ID}</span>
+      <span>{process.env.REACT_APP_NAVER_CLIENT_SECRET}</span>
+      <span>{process.env.REACT_APP_NAVER_CLIENT_ID}</span>
     </div>
   );
 };
